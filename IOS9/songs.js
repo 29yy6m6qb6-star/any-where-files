ea9a2e2f000e67f5677fba27f81998f8439da239
29yy6m6qb6-star <29yy6m6qb6@privaterelay.appleid.com>
2026-05-10 10:11:36 +0900

    Update songs.js

--- IOS9/songs.js
+++ IOS9/songs.js
@@ -1,4 +1,4 @@
 var songs = [
 {title:"とくべチュ、して",artist:"=LOVE",src:"song/とくべチュ、して.mp3"},
-{title:"Song 2",artist:"Artist 2",src:"song/song2.mp3"}
+{title:"fight song",artist:"a",src:"song/fight song.mp3"}
 ];
