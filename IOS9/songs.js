e81f38c21221e1010e7f233b48e31c7d5d8588ed
29yy6m6qb6-star <29yy6m6qb6@privaterelay.appleid.com>
2026-05-10 09:43:10 +0900

    Create songs.js

---
+++ IOS9/songs.js
@@ -0,0 +1,29 @@
+var songs = [
+
+{
+    title:"Song 1",
+    artist:"Artist 1",
+    src:"song1.mp3",
+
+    lyrics:[
+        "Hello",
+        "This is sample lyrics",
+        "iOS9 compatible",
+        "Music player"
+    ]
+},
+
+{
+    title:"Song 2",
+    artist:"Artist 2",
+    src:"song2.mp3",
+
+    lyrics:[
+        "Another song",
+        "More lyrics",
+        "GitHub Pages ready",
+        "Finished"
+    ]
+}
+
+];
