891eb633740bc24923c9d6f437596f839d4122fc
29yy6m6qb6-star <29yy6m6qb6@privaterelay.appleid.com>
2026-05-10 09:51:32 +0900

    Update and rename IOS9/songs.js to IOS9/song/.js

---
+++ IOS9/song/.js
@@ -0,0 +1,5 @@
+var songs = [
+{title:"とくべチュ、して",artist:"=LOVE",src:"song/とくべチュ、して.mp3"},
+{title:"Song 2",artist:"Artist 2",src:"song/song2.mp3"}
+
+];
--- IOS9/songs.js
+++
@@ -1,5 +0,0 @@
-var songs = [
-{title:"とくべチュ、して",artist:"=LOVE",src:"とくべチュ、して.mp3"},
-{title:"Song 2",artist:"Artist 2",src:"song2.mp3"}
-
-];
